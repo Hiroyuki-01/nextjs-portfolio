d54e460d878c81a8728a1d1d6de6d4791562a649
Hiroyuki-01 <hiroyuki0228n@gmail.com>
2023-04-29 00:50:44 +0900

    npm install を実行

--- next.config.js
+++ next.config.js
@@ -1,2 +1,11 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
 module.exports = withNextra()
+
+const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : ''
+module.exports = {
+  // ...
+  assetPrefix: urlPrefix,
+  basePath: urlPrefix,
+  trailingSlash: true,
+  // ...
+};
